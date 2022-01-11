d24493770c62b57f9c02cfd29e05e619e32e1df7
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-01-11 17:38:29 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/nextflow_flutter_init_data_with_provider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nextflow_flutter_init_data_with_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/covid_today.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CovidTodayView extends StatelessWidget {
+  const CovidTodayView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Covid Today'),
+      ),
+      body: Container(),
+    );
+  }
+}
