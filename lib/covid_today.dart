7411050cb7e9d6c7302817985cabc39dd0d55bd9
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-01-11 18:27:39 +0700

    Build example widget that use future from provider
    to determine which UI should be displayed on screen.

--- lib/covid_today.dart
+++ lib/covid_today.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nextflow_flutter_init_data_with_provider/data_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:dio/dio.dart';
 
 class CovidTodayView extends StatelessWidget {
   const CovidTodayView({Key? key}) : super(key: key);
@@ -10,7 +12,17 @@ class CovidTodayView extends StatelessWidget {
       appBar: AppBar(
         title: Text('Covid Today'),
       ),
-      body: Container(),
+      body: FutureBuilder(
+        future: context.read<DataProvider>().loader,
+        builder:
+            (BuildContext context, AsyncSnapshot<Response<dynamic>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Text('ok');
+          }
+
+          return CircularProgressIndicator();
+        },
+      ),
     );
   }
 }
